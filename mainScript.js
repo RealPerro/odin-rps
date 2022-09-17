8e5cb56823dd31142a6077b4cf4b76e7ca6feaa2
RealPerro <chr1@hey.com>
2022-09-17 12:01:51 +0100

    add getcompiuter choice and singleround

---
+++ mainScript.js
@@ -0,0 +1,36 @@
+function getComputerChoice() {
+    let randNum = parseInt(Math.random()*3);
+    const playOptions = ["rock", "paper", "scissors"]
+    return playOptions[randNum];
+}
+
+function singleRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    let result;
+    
+    if (playerSelection === computerSelection) {return "It's a tie";}
+
+    else if (playerSelection == "rock" && computerSelection == "paper") {
+        return `You lose!, ${computerSelection} beats ${playerSelection}.`;}
+    
+    else if (playerSelection == "rock" && computerSelection == "scissors") {
+        return `You win!, ${computerSelection} looses to ${playerSelection}.`;}
+        
+    else if (playerSelection == "paper" && computerSelection == "scissors") {
+        result = `You lose!, ${computerSelection} beats ${playerSelection}.`;
+        return result;}
+
+    else if (playerSelection == "paper" && computerSelection == "rock") {
+        result = `You win!, ${computerSelection} looses to ${playerSelection}.`;
+        return result;}
+
+    else if (playerSelection == "scissors" && computerSelection == "rock") {
+        result = `You lose!, ${computerSelection} beats ${playerSelection}.`;
+        return result;}
+
+    else if (playerSelection == "scissors" && computerSelection == "paper") {
+        result = `You win!, ${computerSelection} looses to ${playerSelection}.`;
+        return result;}
+            
+    else {return "I don't know what is going on :-(";}
+}
